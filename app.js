30d9ed18e220d6c3cba4ac913364b458f5ad485c
Pedro Figueiredo <pedro.figueiredo35@gmail.com>
2023-08-29 15:22:37 +0100

    fixed edit and update plus author routes

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.use("/", indexRoutes);
 const bookRoutes = require("./routes/book.routes"); // <== import (require) book routes
 app.use("/", bookRoutes); // <== use book routes
 
+app.use("/", require("./routes/author.routes"));
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
